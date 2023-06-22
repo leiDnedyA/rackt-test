cfcd27ddf3089b47654a84b193b900adeaeadfee
Ayden Diel <aydendiel@gmail.com>
2023-06-22 15:20:49 -0400

    removed some comments

--- src/app.rkt
+++ src/app.rkt
@@ -17,15 +17,6 @@
                   (header)
                   (gif-controller ([src "./res/cat_dancing.webm"])))))
 
-; (define test-element
-;     (sexp->element `(div ([className "test"]))))
-
-; (define test-element (sexp->react "test"))
-
-; PROBLEM: current pattern doesn't differentiate between [] brackets and () brackets
-; so elements are seen as props
-; (define test-element (sexp->react ("div" ([className "test-div"]) ("p" "test") ("p" "test2"))))
-
 (define test-element (sexp->react (app)))
 
 ; (render (<el app) "root")
